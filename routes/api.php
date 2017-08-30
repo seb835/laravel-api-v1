c279fb105c57552a567e86785eaf6fc1bf9c8b66
Seb Pedley <seb@propeller-studios.co.uk>
2017-08-30 10:19:27 +0100

    Ability to log-in which generates a token

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::put('articles/{article}', 'ArticleController@update');
 Route::delete('articles/{article}', 'ArticleController@delete');
 
 Route::post('register', 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
