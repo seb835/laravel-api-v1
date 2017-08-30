72c0044d0b9848cb301a8d9384019c4d9bde79de
Seb Pedley <seb@propeller-studios.co.uk>
2017-08-30 10:28:33 +0100

    Ability to logout

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -66,10 +66,12 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
+        // if ($request->expectsJson()) {
+        //     return response()->json(['error' => 'Unauthenticated.'], 401);
+        // }
+        //
+        // return redirect()->guest(route('login'));
 
-        return redirect()->guest(route('login'));
+        return response()->json(['error' => 'Unauthenticated'], 401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,18 @@ use App\Http\Controllers\Auth\Request;
 |
 */
 
-Route::get('articles', 'ArticleController@index');
-Route::get('articles/{article}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{article}', 'ArticleController@update');
-Route::delete('articles/{article}', 'ArticleController@delete');
+Route::middleware('auth:api')->get('/user', function(Request $request){
+    return $request->user();
+});
 
 Route::post('register', 'Auth\RegisterController@register');
 Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('articles', 'ArticleController@index');
+    Route::get('articles/{article}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'ArticleController@update');
+    Route::delete('articles/{article}', 'ArticleController@delete');
+});
